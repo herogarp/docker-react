de17f126696fe6f757e77b5171e09f74014b032c
herogarp <jacob.fortunat@gmail.com>
2023-08-02 15:12:46 +0200

    change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Fuck What
+          Fuck What Gith React Shit
         </p>
         <a
           className="App-link"
